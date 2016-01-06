f6cd04c188c59424fa631c19ae323aa66ea1e3c2
Michiaki Mizoguchi <michiaki.mizoguchi@gmail.com>
2016-01-06 22:36:33 +0900

    Add single drag example

---
+++ Assets/Examples/Scripts/CameraPositionHandler.cs
@@ -0,0 +1,30 @@
+﻿using UniRx;
+using UniRx.Triggers;
+using UnityEngine;
+using System.Linq;
+
+[RequireComponent(typeof(TouchObservable))]
+public class CameraPositionHandler : MonoBehaviour
+{
+    [SerializeField]
+    private float moveSpeed = 0.5f;
+
+    [SerializeField]
+    Transform cameraContainerTransform;
+
+    TouchObservable touchObservable;
+
+    void Awake()
+    {
+        touchObservable = GetComponent<TouchObservable>();
+    }
+
+    void Start()
+    {
+        touchObservable.SingleDrag
+            .Select(touch => touch.DeltaPosition)
+            .Where(delta => delta != Vector3.zero)
+            .Select(delta => new Vector3(-delta.x * moveSpeed * 0.05f, 0, -delta.y * moveSpeed * 0.05f))
+            .Subscribe(delta => cameraContainerTransform.position += delta);
+    }
+}
