76d44d698352d14ad95dd226ee6207457529b85f
Michiaki Mizoguchi <michiaki.mizoguchi@gmail.com>
2016-01-09 20:05:36 +0900

    Implement mobile pinch

--- Assets/TouchObservable/Scripts/TouchObservableImplementorMobile.cs
+++ Assets/TouchObservable/Scripts/TouchObservableImplementorMobile.cs
@@ -28,5 +28,35 @@ public class TouchObservableImplementorMobile : TouchObservableImplementor
             .Select(touches => touches.Last() - touches.First())
             .Select(delta => new SingleTouch(new Vector2(Input.mousePosition.x, Input.mousePosition.y), delta))
             .Subscribe(touch => SingleDragStream.OnNext(touch));
+
+        // Pinch
+        var pinching = updateObservable
+            .TakeWhile(_ => Input.touchCount == 2)
+            .Select(_ => Input.touches);
+
+        pinching
+            .Buffer(2, 1)
+            .Where(t => t.Count == 2)
+            .RepeatUntilDestroy(touchObservable)
+            .Subscribe(touches =>
+            {
+                var firstFinger = touches.First().First();
+                var secondFinger = touches.First().Last();
+
+                var previousFirstFinger = touches.Last().Where(t => t.fingerId == firstFinger.fingerId).First();
+                var previousSecondFinger = touches.Last().Where(t => t.fingerId == secondFinger.fingerId).First();
+
+                var firstTouch = new SingleTouch(firstFinger.position, firstFinger.position - previousFirstFinger.position);
+                var secondTouch = new SingleTouch(secondFinger.position, secondFinger.position - previousSecondFinger.position);
+                var doubleTouch = new DoubleTouch(new List<SingleTouch>{firstTouch, secondTouch}, Vector2.Distance(firstFinger.position, secondFinger.position));
+
+                PinchStream.OnNext(doubleTouch);
+            });
+
+        updateObservable
+            .Select(_ => Input.touchCount)
+            .Buffer(2, 1)
+            .Where(c => c.First() == 2 && c.Last() == 1)
+            .Subscribe(_ => PinchEndStream.OnNext(Unit.Default));
     }
 }
