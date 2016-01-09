d25b4553edb9a2a997ed69b6f8a40db8b0f63333
Michiaki Mizoguchi <michiaki.mizoguchi@gmail.com>
2016-01-09 18:14:59 +0900

    Fix example
    - Single drag when rotated
    - Pinch zooming

--- Assets/Examples/Scripts/CameraPositionHandler.cs
+++ Assets/Examples/Scripts/CameraPositionHandler.cs
@@ -30,7 +30,7 @@ public class CameraPositionHandler : MonoBehaviour
         touchObservable.SingleDrag
             .Select(touch => touch.DeltaPosition)
             .Where(delta => delta != Vector2.zero)
-            .Select(delta => new Vector3(-delta.x * moveSpeed * 0.05f, 0, -delta.y * moveSpeed * 0.05f))
+            .Select(delta => cameraContainerTransform.rotation * new Vector3(-delta.x * moveSpeed * 0.05f, 0, -delta.y * moveSpeed * 0.05f))
             .Subscribe(delta => cameraContainerTransform.position += delta);
 
         touchObservable.Pinch
@@ -39,12 +39,12 @@ public class CameraPositionHandler : MonoBehaviour
             .Where(t => t.Count == 2)
             .RepeatUntilDestroy(this)
             .Select(t => t.Last().Distance - t.First().Distance)
-            .Subscribe(delta => cameraContainerTransform.localPosition -= new Vector3(0, 0, delta * moveSpeed * 0.05f));
+            .Subscribe(delta => transform.localPosition -= new Vector3(0, delta * moveSpeed * 0.05f, -delta * moveSpeed * 0.05f));
 
         touchObservable.DoubleDrag
             .Select(t => t.Touches[0].DeltaPosition)
             .Where(p => p != Vector2.zero)
-            .Select(d => new Vector3(Mathf.Clamp(cameraContainerTransform.eulerAngles.x - d.y * swipeTurnSpeed * 0.05f, 0, 90), cameraContainerTransform.eulerAngles.y + d.x * swipeTurnSpeed * 0.05f, 0))
+            .Select(d => new Vector3(Mathf.Clamp(cameraContainerTransform.eulerAngles.x - d.y * swipeTurnSpeed * 0.3f, 0, 65), cameraContainerTransform.eulerAngles.y + d.x * swipeTurnSpeed * 0.3f, 0))
             .Subscribe(e => cameraContainerTransform.eulerAngles = e);
 
     }
